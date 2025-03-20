34b97e77cce7df7178c21832242a68d82192f43d
VidalCamargos <vidalcamargos@gmail.com>
2025-03-19 21:56:18 -0300

    fix content type

--- tests/Unit/Http/Middleware/AlwaysReturnJsonTest.php
+++ tests/Unit/Http/Middleware/AlwaysReturnJsonTest.php
@@ -8,7 +8,7 @@ class AlwaysReturnJsonTest extends TestCase
 {
     public function test_it_will_apply_content_type_application_json_properly(): void
     {
-        $this->getJson(route('api.ping'), ['Content-Type' => 'text/HTML'])
+        $this->getJson(route('api.ping'), ['Content-Type' => 'text/html'])
             ->assertHeader('Content-Type', 'application/json');
     }
 }
