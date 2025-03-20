91be2e1032a5027e43d1e1c420df2162b6bfdda0
VidalCamargos <vidalcamargos@gmail.com>
2025-03-19 22:35:43 -0300

    install and run ecs

---
+++ ecs.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return ECSConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/config',
+        __DIR__.'/database',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ])
+    ->withRules([
+        NoUnusedImportsFixer::class,
+    ])
+    ->withPreparedSets(
+        psr12: true,
+     );
