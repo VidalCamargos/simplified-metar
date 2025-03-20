9d3a3cb3d595b0f52c9f4cc4df7698a5a5f6e7ea
VidalCamargos <vidalcamargos@gmail.com>
2025-03-19 21:55:46 -0300

    remove frontend, add ping route, add middleware to force json and test

---
+++ tests/Unit/Http/Middleware/AlwaysReturnJsonTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Http\Middleware;
+
+use Tests\TestCase;
+
+class AlwaysReturnJsonTest extends TestCase
+{
+    public function test_it_will_apply_content_type_application_json_properly(): void
+    {
+        $this->getJson(route('api.ping'), ['Content-Type' => 'text/HTML'])
+            ->assertHeader('Content-Type', 'application/json');
+    }
+}
